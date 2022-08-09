f51f506df7ec7ee880c1e79419f9e8cfaaa1f694
leoferraari <leonardo.5567@hotmail.com>
2022-08-09 17:11:55 -0300

    teste
    teste

--- projeto/resources/views/projeto/create.blade.php
+++ projeto/resources/views/projeto/create.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="imagem/png" href="" />
-    <title>TCC</title>
-    <link href="{{ asset('/styles.css') }}" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    @yield('style')
-</head>
-    <body>
+@include("header")
         <div class="container">
             <div class="d-flex justify-content-center align-items-center">
                 <div class="card mt-5">
@@ -215,6 +202,5 @@
                 });
             }
         </script>
-    </body> 
-</html>
+
 
